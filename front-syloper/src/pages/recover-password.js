75f507ec671842ed6735d53ee5c7c3aa295431b8
pepiyu <58489884+pepiyu@users.noreply.github.com>
2021-12-25 13:56:06 +0100

    refactor(login): refactorize styled-components

--- front-syloper/src/pages/recover-password.js
+++ front-syloper/src/pages/recover-password.js
@@ -1,27 +1,15 @@
 import React from "react"
 import Layout from "../components/layout";
-import styled from "styled-components"
+import { MainLoginStyled}  from "./styles/MainLoginStyled"
 import RecoverPassword from "../components/Login/RecoverPassword";
-
-// REFACTORIZAR
-
-const MainLogin = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  align-content: flex;
-  justify-content: center;
-  background-color: #f8f9fa;
-`;
 
 
 const IndexPage = () => {
   return (
     <Layout>
-      <MainLogin>
+      <MainLoginStyled>
         <RecoverPassword />
-      </MainLogin>
+      </MainLoginStyled>
     </Layout>
      
   )
