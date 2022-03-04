3a38ca7632f98f56c279dc7a644af381564e1807
pepiyu <58489884+pepiyu@users.noreply.github.com>
2022-03-04 19:48:50 +0100

    :clown_face: mock ups

--- front-syloper/src/components/Home/ProjectDetail/DetailContainer/Tasks/index.js
+++ front-syloper/src/components/Home/ProjectDetail/DetailContainer/Tasks/index.js
@@ -6,7 +6,7 @@ import { SelectStatus, StatusButton, TaskColumn, TaskContainer, TaskScroll } fro
 import Task from './Task';
 import classNames from 'classnames';
 import { useViewport } from '../../../../../contexts/viewportSize';
-
+import tasksData from './tasksData.json';
 const Tasks = ({ project_id }) => {
 
     const [ activeStatus, setActiveStatus ] = useState("status1");
@@ -18,57 +18,6 @@ const Tasks = ({ project_id }) => {
         setActiveStatus(name);
     }
     
-    const tasksData = [    
-        {
-            project_id: "61ca2304422d3f1042b6f34a",
-            task_date: "2021-12-30T16:46:18.000Z",
-            task_due_date: "2022-01-30T16:46:18.000Z",
-            task_description: "Esta es una descripcion de ejemplo para esta tarea de ejemplo",
-            task_responsable_id: "91ca2304732d3f1542b6f3a4",
-            createdAt: "2021-12-30T16:54:41.377Z",
-            updatedAt: "2021-12-30T16:54:41.377Z",
-            id: "61cde451c9973bbc0ce7de9f",
-            title: "do something",//new
-            status: 0 //new
-        },
-        {
-            project_id: "61ca2304422d3f1042b6f34a",
-            task_date: "2021-12-30T16:49:00.000Z",
-            task_due_date: "2022-01-30T21:00:00.000Z",
-            task_description: "Esta es una descripcion de ejemplo para esta tarea de ejemplo",
-            task_responsable_id: "91ca2302832d3f1542b6f3a4",
-            createdAt: "2021-12-30T16:55:52.380Z",
-            updatedAt: "2021-12-30T16:55:52.380Z",
-            id: "61cde498c9973bbc0ce7dea1",
-            title: "do something",
-            status: 0
-        },
-        {
-            project_id: "61ca2304422d3f1042b6f34a",
-            task_date: "2021-12-30T16:49:00.000Z",
-            task_due_date: "2022-01-30T21:00:00.000Z",
-            task_description: "Esta es una descripcion de ejemplo para esta tarea de ejemplo",
-            task_responsable_id: "91ca2302832d3f1542b6f3a4",
-            createdAt: "2021-12-30T16:55:52.380Z",
-            updatedAt: "2021-12-30T16:55:52.380Z",
-            id: "61cde498c9973bbc0ce7dea1",
-            title: "do something",
-            status: 1
-        },
-        {
-            project_id: "61ca2304422d3f1042b6f34a",
-            task_date: "2021-12-30T16:49:00.000Z",
-            task_due_date: "2022-01-30T21:00:00.000Z",
-            task_description: "Esta es una descripcion de ejemplo para esta tarea completada",
-            task_responsable_id: "91ca2302832d3f1542b6f3a4",
-            createdAt: "2021-12-30T16:55:52.380Z",
-            updatedAt: "2021-12-30T16:55:52.380Z",
-            id: "61cde498c9973bbc0ce7dea1",
-            title: "do something",
-            status: 2
-        },
-      ]
-
     const ColumnsDesktop = () => 
         <>
             <TaskColumn>
