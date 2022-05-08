7eebd4af075d38fd890d885ae8836303611e4760
Josue Lopez <jomalolep@gmail.com>
2022-05-08 02:14:22 -0500

    feature: portfolio endpoint data

--- packages/front-syloper/src/components/Home/Portfolios/Table/index.js
+++ packages/front-syloper/src/components/Home/Portfolios/Table/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TableBody from './styles';
+import ServiceBFF from '../../../../services/BFF';
 
 const Table = () => {
+  const [tableData, setTableDta] = useState();
+
+  useEffect(() => {
+    ServiceBFF.getPortfolio().then((data) => setTableDta(data));
+  }, []);
+
   return (
     <TableBody>
       <tr>
@@ -13,43 +20,17 @@ const Table = () => {
         <th>Estimated Incomes</th>
         <th>Real Incomes</th>
       </tr>
-
-      <tr>
-        <td>Web App Development</td>
-        <td>Pepsi</td>
-        <td>$200</td>
-        <td>48h</td>
-        <td>25h</td>
-        <td>$1,600</td>
-        <td>$1,800</td>
-      </tr>
-      <tr>
-        <td>Web App Development</td>
-        <td>Pepsi</td>
-        <td>$200</td>
-        <td>48h</td>
-        <td>25h</td>
-        <td>$1,600</td>
-        <td>$1,800</td>
-      </tr>
-      <tr>
-        <td>Web App Development</td>
-        <td>Pepsi</td>
-        <td>$200</td>
-        <td>48h</td>
-        <td>25h</td>
-        <td>$1,600</td>
-        <td>$1,800</td>
-      </tr>
-      <tr>
-        <td>Web App Development</td>
-        <td>Pepsi</td>
-        <td>$200</td>
-        <td>48h</td>
-        <td>25h</td>
-        <td>$1,600</td>
-        <td>$1,800</td>
-      </tr>
+      {tableData?.map((row) => (
+        <tr>
+          <td>{row.project_title}</td>
+          <td>{row.client}</td>
+          <td>{row.price_per_hour}</td>
+          <td>{row.estimated_hours}</td>
+          <td>{row.worked_hours}</td>
+          <td>{row.estimated_incomes}</td>
+          <td>{row.real_incomes}</td>
+        </tr>
+      ))}
     </TableBody>
   );
 };
