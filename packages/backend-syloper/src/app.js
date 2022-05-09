797a6d7512ee03d1959cff52ef84097374786fe2
andres.maldonado <andres.maldonado@wom.cl>
2022-05-09 19:14:14 -0400

    feat: new project

--- packages/backend-syloper/src/app.js
+++ packages/backend-syloper/src/app.js
@@ -4,8 +4,11 @@ const koaBody = require('koa-body');
 const appRoutes = require('./routes');
 const cors = require('@koa/cors');
 const authentication = require('./middleware/authentication');
+const serve = require('koa-static');
 
 const app = new Koa();
+
+app.use(serve('./public'));
 
 // Middlewares
 app.use(cors());
